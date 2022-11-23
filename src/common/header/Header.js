aa98c9e6789d3f35e9fa7d35c941acb458ea59db
DHRUV-LIMBACHIYA <dhruv.mail4work@gmail.com>
2022-11-23 22:40:08 +0530

    added header login/logout button with styles

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,17 +1,27 @@
 import React from "react";
 import './Header.css';
 import AppLogo from '../../assets/logo.svg';
+import { Button } from "@material-ui/core";
 
 const Header = () => {
+
+    const isLoggedIn = sessionStorage.getItem('access_token')
+
+    let headerButton;
+
+    if (isLoggedIn) {
+        headerButton = <Button variant='contained'>Logout</Button>
+    } else {
+        headerButton = <Button variant='contained'>Login</Button>
+    }
 
     return (
         <div className="header">
             <div>
-                <img src={AppLogo} className='app-logo rotate-anim'/>
+                <img src={AppLogo} className='app-logo rotate-anim' />
             </div>
             <div>
-                <button>Login</button>
-                <button>Logout</button>
+                {headerButton}
             </div>
         </div >
     )
