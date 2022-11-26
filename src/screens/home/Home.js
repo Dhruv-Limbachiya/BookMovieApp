d83a97bc5ebc58ba6ab247ea2710f9cdff9e825d
DHRUV-LIMBACHIYA <dhruv.mail4work@gmail.com>
2022-11-26 16:29:37 +0530

    added upcoming movies section

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../common/header/Header";
 import './Home.css';
+import { makeStyles } from "@mui/styles";
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+
 
 const Home = () => {
+
+    const [upcomingMovies, setUpcomingMovies] = useState([])
+
+    useEffect(() => {
+        getUpcomingMovies()
+    }, [])
+
+    const getUpcomingMovies = async () => {
+        try {
+            const status = 'PUBLISHED';
+            const rawResponse = await fetch(`http://localhost:8085/api/v1/movies?status=${status}&limit=6`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json;charset=UTF-8",
+                    "Accept": "application/json;charset=UTF-8",
+                },
+            })
+
+            const response = await rawResponse.json()
+
+            if (rawResponse.ok) {
+                setUpcomingMovies(response.movies);
+            } else {
+                throw (new Error(response.message || 'Something went wrong!'))
+            }
+        } catch (e) {
+            alert(`Error: ${e.message}`);
+        }
+    }
+
     return (
         <div>
             <Header />
@@ -10,6 +45,26 @@ const Home = () => {
                 <div id='upcoming-movies-heading'>
                     Upcoming Movies
                 </div>
+            </div>
+            <div>
+                <ImageList
+                    rowHeight={250}
+                    sx={{
+                        gridAutoFlow: "column",
+                        gridTemplateColumns: "repeat(auto-fill,minmax(250px,1fr)) !important",
+                        gridAutoColumns: "minmax(250px, 1fr)",
+                        margin: 'auto',
+                        overflowY:'hidden'
+                    }}>
+                    {
+                        upcomingMovies.map((movie) => (
+                            <ImageListItem key={movie.id}>
+                                <img src={movie.poster_url} />
+                                <ImageListItemBar title={movie.title} />
+                            </ImageListItem>
+                        ))
+                    }
+                </ImageList>
             </div>
         </div>
     )
